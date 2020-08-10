b5fcc59b5d575367d30edee12921371f64d5c319
Esteban Salas <esalasestrada@gmail.com>
2020-08-10 13:07:30 -0600

    primer avance, tratar de quitar el IO

--- main.hs
+++ main.hs
@@ -1,4 +1,30 @@
+import Prelude
 import System.IO
+import Data.Char 
+import Data.HashSet as HashSet hiding (sort, map) 
+import Data.List (sort, map)
+
+type NotSignificant = HashSet [Char]
+
+getNotSignificantWords :: Handle -> IO NotSignificant
+getNotSignificantWords inh = 
+  do
+    words <- getNotSignificantWordsAux inh []
+    let hashpals = HashSet.fromList words
+    return hashpals
+    
+
+getNotSignificantWordsAux :: Handle -> [[Char]] -> IO [[Char]]
+getNotSignificantWordsAux inh words =  
+  do
+    ineof <- hIsEOF inh
+    if ineof then do
+      return words
+    else do
+      inpStr <- hGetLine inh
+      let minus = map toLower inpStr
+      getNotSignificantWordsAux inh (words ++ [minus])
+      
 
 main :: IO ()
 main = do
@@ -6,6 +32,6 @@ main = do
   let tokens = words inpStr
   let filename = head tokens
   inh <- openFile filename ReadMode
-  f filename
-
-f a = putStrLn a
+  let hashNotSignificants = getNotSignificantWords inh
+  hClose inh
+  putStrLn (show hashNotSignificants)
