76afb2e4521b6f0dedcb37365bfe63b031b1127c
Esteban Mata <esteban1113001@gmail.com>
2020-08-11 10:55:47 -0600

    Rotelo bro

--- main.hs
+++ main.hs
@@ -1,36 +1,16 @@
-import Data.Char
-import Data.HashSet as HashSet hiding (map, sort)
-import Data.List (map, sort)
+import Files
+import Rotations
 import System.IO
-import Prelude
-
-type NotSignificant = HashSet [Char]
-
-getNotSignificantWords :: Handle -> IO NotSignificant
-getNotSignificantWords inh =
-  do
-    words <- getNotSignificantWordsAux inh []
-    let hashpals = HashSet.fromList words
-    return hashpals
-
-getNotSignificantWordsAux :: Handle -> [[Char]] -> IO [[Char]]
-getNotSignificantWordsAux inh words =
-  do
-    ineof <- hIsEOF inh
-    if ineof
-      then do
-        return words
-      else do
-        inpStr <- hGetLine inh
-        let minus = map toLower inpStr
-        getNotSignificantWordsAux inh (words ++ [minus])
 
 main :: IO ()
 main = do
   inpStr <- getLine
   let tokens = words inpStr
   let filename = head tokens
-  inh <- openFile filename ReadMode
-  hashNotSignificants <- getNotSignificantWords inh
-  hClose inh
-  putStrLn (show hashNotSignificants)
+  ns <- openFile filename ReadMode
+  hashNotSignificants <- getNotSignificantWords ns
+  hClose ns
+  ti <- openFile (tokens !! 1) ReadMode
+  titlesList <- getTitles ti
+  hClose ti
+  putStrLn (show (titSigRotations (titlesList !! 23) hashNotSignificants))
